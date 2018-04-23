f3b9886826c93eb937549bebca23be498fc14b2b
vincent <vpark45@gmail.com>
2018-04-23 14:13:41 +0900

    게시글 전체 공개 설정 추가
    기본 값이 회원만 볼 수 있게 되어 있어서 전체 공개 값을 기본 값으로 전달함

--- cafe.py
+++ cafe.py
@@ -7,14 +7,14 @@ def enc(x):
     x = quote(x)
     return quote(x, encoding='cp949')
 
-def write(access_token, clubid, menuid, subject, content):
+def write(access_token, clubid, menuid, subject, content, openyn='true'):
     url = 'https://openapi.naver.com/v1/cafe/%s/menu/%s/articles'
     url = url % (clubid, menuid)
     headers = {'Authorization' : 'Bearer ' + access_token}
     subject = enc(subject)
     content = md2html(content)
     content = enc(content)
-    data = 'subject=%s&content=%s' % (subject, content)
+    data = 'subject=%s&content=%s&openyn=%s' % (subject, content, openyn)
     data = data.encode('ascii')
     req = Request(url, headers=headers, data=data)
     urlopen(req)
